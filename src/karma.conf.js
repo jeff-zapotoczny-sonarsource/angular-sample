de20f84b2c988475d7de0732dd0e8859ba953664
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2019-11-05 13:58:25 -0600

    Increasing test timeout.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    browserNoActivityTimeout: 400000,
     singleRun: true,
     restartOnFileChange: true
   });
