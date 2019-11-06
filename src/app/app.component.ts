66a80fa69515643b887362445500962e31b205d8
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2019-11-06 15:31:21 -0600

    Adding comments.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+// This is a pretty generic component added for test purposes
+// this is another comment line
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
